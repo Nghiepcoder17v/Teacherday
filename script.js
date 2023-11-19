2c8b7dcb5ea8854829ba3de9adb7bcbd70e6daf7
Ctrl-Shift-N <havannghiep7798@gmail.com>
2023-11-19 23:55:01 +0700

    pussh1

---
+++ script.js
@@ -0,0 +1,16 @@
+function over ()
+{document.getElementById("img").src="https://rukminim1.flixcart.com/image/416/416/mug/s/f/e/1-lolprint-meaning-teachers-day-original-imaeae64b9yufevc.jpeg?q=70"
+
+  }
+function leave (){ document.getElementById("img").src="https://static.toiimg.com/photo/msid-70948645/70948645.jpg?100335"
+}
+
+function function7() {
+window.alert("Want to play music.") ;
+  document.getElementById("audio").play()
+}
+
+function divelem() {
+  window.alert("Want to pause music") ;
+  document.getElementById("audio").pause()
+}
